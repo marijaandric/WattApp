b4ce68da007610711f2dcca9469277c284706adb
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-03-02 01:27:14 +0100

    demo_app_final_commit

---
+++ sandbox/denis_dragoljubovic/back/API/Data/UserDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
