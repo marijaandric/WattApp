4249d607836f1c9b3631552f220374939c01c4a2
pandur035 <stojanoviccaleksa@gmail.com>
2023-03-09 01:29:42 +0100

    Added connection with database
    - added connection with database
    - created user model

---
+++ wattApp/backend/backend/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+
+    }
+}
