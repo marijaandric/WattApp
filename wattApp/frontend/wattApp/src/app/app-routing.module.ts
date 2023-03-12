2fd6ac6f28117333c6c2de900820c6752d28a581
Boris <dugicboris@gmail.com>
2023-03-12 21:57:50 +0100

    Contact page
    Finished Contact us page

--- wattApp/frontend/wattApp/src/app/app-routing.module.ts
+++ wattApp/frontend/wattApp/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/global/login/login.component';
 import { NavbarComponent } from './components/global/navbar/navbar.component';
 import { LandingPageComponent } from './components/global/landing-page/landing-page.component';
+import { ContactComponent } from './components/global/contact/contact.component';
 
 const routes: Routes = [
   {path:'login', component:LoginComponent},
   {path:'', component: LandingPageComponent},
+  {path:'contact', component:ContactComponent},
 ];
 
 @NgModule({
