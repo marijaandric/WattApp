5b4ac91ef1f697c92d84dd03cb61f8a3e5a58558
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-03-12 18:19:04 +0100

    Sidebar finished

--- wattApp/frontend/wattApp/src/app/components/global/layout-components/side-bar/sidebar.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/layout-components/side-bar/sidebar.component.ts
@@ -1,31 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
-  list = [
-    {
-      number: '1',
-      name:'Home',
-      icon:'fa-solid fa-house',
-    },
-    {
-      number: '2',
-      name:'Users',
-      icon:'fa fa-users',
-    },
-    {
-      number: '1',
-      name:'Dashboard',
-      icon:'fa-solid fa-house',
-    },
-    {
-      number: '1',
-      name:'Settings',
-      icon:'fa-solid fa-house',
-    },
-  ];
+export class SidebarComponent implements OnInit {
+  isSidebarShown: boolean = false;
+  isSubMenuShown: boolean = false;
+  isSubMenu2Shown: boolean = false;
+  hostElement: HTMLElement | undefined;
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+    this.hostElement = this.elementRef.nativeElement as HTMLElement;
+    console.log(':host selector:', this.hostElement.tagName.toLowerCase());
+  }
+
+  showSidebar() {
+    this.isSidebarShown = !this.isSidebarShown;
+    this.hostElement?.classList.toggle('close', this.isSidebarShown);
+  }
+
+  showSubMenus() {
+    this.isSubMenuShown = !this.isSubMenuShown;
+    console.log(this.isSubMenuShown);
+  }
+
+  showSubMenus2() {
+    this.isSubMenu2Shown = !this.isSubMenu2Shown;
+    console.log(this.isSubMenu2Shown);
+  }
 }
+/*import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+  isSidebarShown: boolean = false;
+  isSubMenuShown: boolean = false;
+  isSubMenu2Shown: boolean = false;
+
+  showSidebar(){
+    
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".menu");
+    console.log(sidebarBtn);
+    sidebar?.classList.toggle("close");
+    let sidebar = document.
+    this.isSidebarShown = !this.isSidebarShown;
+  }
+
+  showSubMenus() {
+    this.isSubMenuShown = !this.isSubMenuShown;
+    console.log(this.isSubMenuShown);
+  }
+
+  showSubMenus2() {
+    this.isSubMenu2Shown = !this.isSubMenu2Shown;
+    console.log(this.isSubMenu2Shown);
+  }
+}
+*/
