a8f96013ac718f2a1df63e12f9d80a66c2d6ffce
pandur035 <stojanoviccaleksa@gmail.com>
2023-03-13 17:55:13 +0100

    Updated controllers
    Added endpoints for user

--- wattApp/backend/backend/Controllers/UserController.cs
+++ wattApp/backend/backend/Controllers/UserController.cs
@@ -25,6 +25,27 @@ namespace backend.Controllers
             _context = context;
         }
 
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/proba/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
         // POST - AUTENTIFIKACIJA
         [HttpPost("authenticate")]
         public async Task<IActionResult> AuthenticateUser([FromBody] User userObj)
@@ -72,11 +93,55 @@ namespace backend.Controllers
             return Ok(new { Message = "User Registered!" });
         }
 
-        [HttpGet]
-        public async Task<ActionResult<User>> GetAllUsers()
+        // DELETE: api/proba/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
         {
-            return Ok( await _context.Users.ToListAsync());
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
-        
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
     }
 }
