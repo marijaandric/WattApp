ecc580fbc209487947da185c3365444267b942d9
Boris <dugicboris@gmail.com>
2023-03-14 23:42:59 +0100

    css changes
    little css changes

--- wattApp/frontend/wattApp/src/app/components/prosumer/home-page-components/promotion/promotion.component.ts
+++ wattApp/frontend/wattApp/src/app/components/prosumer/home-page-components/promotion/promotion.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, HostBinding, Input } from '@angular/core';
 })
 export class PromotionComponent implements OnInit{
   subTitle : String ="Get a discount!";
-  Title : String ="Show us all your device, get a 10% discount! For more information, contact our operators!";
+  Title : String ="Show us all your device, get a 10% discount! For more information, contact our operators! Dear consumers, we inform you that the price of electricity will decrease by 5% in the coming period.";
 
   @Input() @HostBinding("blue-color") public isBlue = false;
   @Input() @HostBinding("bg-blue-color") public isBgBlue = false;
