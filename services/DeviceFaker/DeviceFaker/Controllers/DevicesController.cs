c17b643ee52cf22e14a36f0522992c6652d1b59e
pandur035 <stojanoviccaleksa@gmail.com>
2023-03-14 20:58:55 +0100

    Feature update
    - Created mock dataset for Devices and Devices Data
    - Created REST API for DevicesData mock service
    -

---
+++ services/DeviceFaker/DeviceFaker/Controllers/DevicesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DeviceFaker.Context;
+using DeviceFaker.Models;
+
+namespace DeviceFaker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DevicesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Devices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Devices>>> GetDevices()
+        {
+          if (_context.Devices == null)
+          {
+              return NotFound();
+          }
+            return await _context.Devices.ToListAsync();
+        }
+
+        // GET: api/Devices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Devices>> GetDevices(int id)
+        {
+          if (_context.Devices == null)
+          {
+              return NotFound();
+          }
+            var devices = await _context.Devices.FindAsync(id);
+
+            if (devices == null)
+            {
+                return NotFound();
+            }
+
+            return devices;
+        }
+
+        // PUT: api/Devices/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDevices(int id, Devices devices)
+        {
+            if (id != devices.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(devices).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DevicesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Devices
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Devices>> PostDevices(Devices devices)
+        {
+          if (_context.Devices == null)
+          {
+              return Problem("Entity set 'AppDbContext.Devices'  is null.");
+          }
+            _context.Devices.Add(devices);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDevices", new { id = devices.Id }, devices);
+        }
+
+        // DELETE: api/Devices/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDevices(int id)
+        {
+            if (_context.Devices == null)
+            {
+                return NotFound();
+            }
+            var devices = await _context.Devices.FindAsync(id);
+            if (devices == null)
+            {
+                return NotFound();
+            }
+
+            _context.Devices.Remove(devices);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DevicesExists(int id)
+        {
+            return (_context.Devices?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ services/DeviceFaker/DeviceFaker/Migrations/20230314192858_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeviceFaker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DeviceID = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserID = table.Column<int>(type: "INTEGER", nullable: false),
+                    DeviceName = table.Column<string>(type: "TEXT", nullable: false),
+                    Room = table.Column<string>(type: "TEXT", nullable: false),
+                    DeviceType = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
