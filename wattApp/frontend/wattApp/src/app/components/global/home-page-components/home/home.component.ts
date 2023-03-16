1a52026ff0c48f395d6e8efacb8c82e0178e4019
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-03-16 21:18:45 +0100

    #33 Dialog redesigned

--- wattApp/frontend/wattApp/src/app/components/global/home-page-components/home/home.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/home-page-components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { StadardTemplateComponent } from '../../layout-components/standard-template/stadard-template.component';
 
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
@@ -8,7 +9,8 @@ import { StadardTemplateComponent } from '../../layout-components/standard-templ
 })
 export class HomeComponent {
   display: boolean = false;
-
+  menageUserForm! : FormGroup;
+  
   showDialog() {
       this.display = true;
   }
