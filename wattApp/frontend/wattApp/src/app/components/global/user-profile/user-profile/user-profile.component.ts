d99dac360edac2ce8666b15be68146fdbac11aa6
Marija Andric <marijaandric2001@gmail.com>
2023-03-17 14:29:27 +0100

    Updated profile page design

--- wattApp/frontend/wattApp/src/app/components/global/user-profile/user-profile/user-profile.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/user-profile/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
+  subTitle : String ="Get a discount!";
+  Title : String ="Show us all your device, get a 10% discount! For more information, contact our operators! Dear consumers, we inform you that the price of electricity will decrease by 5% in the coming period.";
 
+  @Input() @HostBinding("blue-color") public isBlue = false;
+  @Input() @HostBinding("bg-blue-color") public isBgBlue = false;
+  
+  @Input() @HostBinding("yellow-color") public isYellow = false;
+  @Input() @HostBinding("bg-yellow-color") public isBgYellow = false;
+  
+  @Input() @HostBinding("pink-color") public isPink = false;
+  @Input() @HostBinding("bg-pink-color") public isBgPink = false;
 }
