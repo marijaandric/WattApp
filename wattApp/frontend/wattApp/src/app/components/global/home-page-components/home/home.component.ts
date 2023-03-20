ffbb97274e734e637533222d8cc306e2d0ce7803
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-03-20 23:36:11 +0100

    #40 Angular Dialog - add, menage device

--- wattApp/frontend/wattApp/src/app/components/global/home-page-components/home/home.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/home-page-components/home/home.component.ts
@@ -4,6 +4,7 @@ import { StadardTemplateComponent } from '../../layout-components/standard-templ
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { ConfirmationService } from 'primeng/api';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
@@ -12,9 +13,10 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 export class HomeComponent {
   display: boolean = false;
   display2: boolean = false;
+  display3: boolean = false;
   menageUserForm! : FormGroup;
   
-  constructor(private userService:UserService, private authService:AuthService){}
+  constructor(private userService:UserService, private authService:AuthService) {}
 
   showDialog() {
       this.display = true;
@@ -22,7 +24,10 @@ export class HomeComponent {
 
   showDialog2() {
     this.display2 = true;
-}
+  }
+  showDialog3() {
+    this.display3 = true;
+  }
 
   isAdmin()
   {
