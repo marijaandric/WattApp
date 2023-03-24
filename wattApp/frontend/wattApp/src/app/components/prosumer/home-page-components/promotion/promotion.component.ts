99b6f225db88474c032c17609babd223465e414a
Boris <dugicboris@gmail.com>
2023-03-24 23:45:45 +0100

    First look
    First look for Prosumer Home

--- wattApp/frontend/wattApp/src/app/components/prosumer/home-page-components/promotion/promotion.component.ts
+++ wattApp/frontend/wattApp/src/app/components/prosumer/home-page-components/promotion/promotion.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit, HostBinding, Input } from '@angular/core';
   styleUrls: ['./promotion.component.css']
 })
 export class PromotionComponent implements OnInit{
-  subTitle : String ="Get a discount!";
-  Title : String ="Show us all your device, get a 10% discount! For more information, contact our operators! Dear consumers, we inform you that the price of electricity will decrease by 5% in the coming period.";
+  @Input() subTitle : String ="Get a discount!";
+  @Input() Title : String ="Show us all your device, get a 10% discount! For more information, contact our operators! Dear consumers, we inform you that the price of electricity will decrease by 5% in the coming period.";
 
   @Input() @HostBinding("blue-color") public isBlue = false;
   @Input() @HostBinding("bg-blue-color") public isBgBlue = false;
