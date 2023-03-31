e1666a355f0c706370b85dbe162ca4df4b0d731e
Boris <dugicboris@gmail.com>
2023-03-31 13:39:26 +0200

    Statistic-PieChart

--- wattApp/frontend/wattApp/src/app/components/Prosumer/statistic-page-components/statistic/statistic.component.ts
+++ wattApp/frontend/wattApp/src/app/components/Prosumer/statistic-page-components/statistic/statistic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 interface City {
   name: string,
   code: string
@@ -12,14 +13,16 @@ interface City {
 export class StatisticComponent  implements OnInit {
   type: City[];
   selectedType!: City;
-  constructor() {
+
+  constructor(private http: HttpClient) {
     this.type = [
       {name: 'Consumption', code: '1'},
       {name: 'Production', code: '2'},
-      {name: 'Stock', code: '3'},
       {name: 'All', code: '4'},
   ];
   }
+
+  
   barchartHeight=250;
 
   piechartHeight1=300;
@@ -29,9 +32,30 @@ export class StatisticComponent  implements OnInit {
   text3='Producers per room';
   text4='Storage per room';
 
-  niz1=[50,10,20,30];
-  niz2=[40, 32, 28, 15];
+
+  niz1=[];
+  niz2=[6,3,3,3];
   niz3=[10,20,20,1];
   niz4=[15,15,20,30];
-  ngOnInit(): void {}
+
+
+  rooms: string[] =[];
+  count: number[]=[];
+
+  getDevicePerRoom() {
+    const deviceId = 1 ;
+   // const deviceId = this.user.id ;
+    const type = 'Consumer';
+    const number = 4;
+    this.http.get<{rooms: string[], count: number[]}>(`https://localhost:7158/api/Devices/chart/${deviceId}/${type}/${number}`).subscribe(data => {
+      this.rooms = data.rooms;
+      this.count = data.count;
+     // this.niz1 = data.count;
+      console.log(data);
+    });
   }
+  
+ ngOnInit(): void {
+   this.getDevicePerRoom();
+  }
+}
