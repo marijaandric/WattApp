9c151387919bba65dfd17639d77857ee2f0775fd
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-04-04 00:28:38 +0200

    #71 Marker for map design

---
+++ wattApp/frontend/wattApp/src/app/components/DSO/marker/marker.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marker',
+  templateUrl: './marker.component.html',
+  styleUrls: ['./marker.component.css']
+})
+export class MarkerComponent implements OnInit{
+  
+  @Input() @HostBinding("blue-color") public isBlue = false;
+  @Input() @HostBinding("bg-blue-color") public isBgBlue = false;
+  
+  @Input() @HostBinding("yellow-color") public isYellow = false;
+  @Input() @HostBinding("bg-yellow-color") public isBgYellow = false;
+  
+  @Input() @HostBinding("pink-color") public isPink = false;
+  @Input() @HostBinding("bg-pink-color") public isBgPink = false;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
