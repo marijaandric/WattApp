8059da810b3c38e79a7dc4a45b23440eea7f866a
pandur035 <stojanoviccaleksa@gmail.com>
2023-04-06 00:33:30 +0200

    Changed a type for authorid

---
+++ wattApp/backend/backend/Migrations/20230405223247_updatedDsoNews.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedDsoNews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "News");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "News",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "News");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "News",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
