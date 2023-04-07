2112962f3c66fb79c7412230e369d41124b2b8bc
Marija Andric <marijaandric2001@gmail.com>
2023-04-07 21:15:24 +0200

    History and forecast line chart media query for statistics page

--- wattApp/frontend/wattApp/src/app/components/Prosumer/statistic-page-components/statistic/statistic.component.ts
+++ wattApp/frontend/wattApp/src/app/components/Prosumer/statistic-page-components/statistic/statistic.component.ts
@@ -7,6 +7,10 @@ interface City {
   name: string,
   code: string
 }
+interface SwitchOption {
+  label: string;
+  value: boolean;
+}
 
 @Component({
   selector: 'app-statistic',
@@ -16,6 +20,20 @@ interface City {
 export class StatisticComponent  implements OnInit {
   type: City[];
   selectedType!: City;
+  @ViewChild('myChart', { static: true }) myChart! : PieChartComponent;
+
+  switchValue: boolean = true;
+
+  switchOptions: SwitchOption[] = [
+    {label: 'History', value: true},
+    {label: 'Forecast', value: false}
+  ];
+
+
+  niz1 = [];
+  niz2 = [6,3,3,3];
+  niz3 : number[] = new Array(4);   //[1,2,3,4]
+  niz4 = [15,15,20,30];
   
   barchartHeight=250;
 
@@ -26,30 +44,31 @@ export class StatisticComponent  implements OnInit {
   text3='Producers per room';
   text4='Storage per room';
 
+
   rooms: string[] =[];
   count: number[]=[];
+
 
   constructor(private http: HttpClient, private deviceService : DeviceService) {
     this.type = [
       {name: 'Consumption', code: '1'},
       {name: 'Production', code: '2'},
-      {name: 'Stock', code: '3'},
       {name: 'All', code: '4'},
     ];
   }
-  findConsumer(code: string): string {
-    const type = this.type.find(item => item.code === code);
-    return 'Consumer';
-  }
+
 
   getDevicePerRoom(){
-  const id = 1 ;
+    const deviceId = 1 ;
    // const deviceId = this.user.id ;
-  const type = this.findConsumer('1');
-  const number = 4;
-  this.deviceService.devicesPerRooms(id, type, number).subscribe(data => {
+    const type = 'Consumer';
+    const number = 4;
+    this.deviceService.devicesPerRooms(deviceId, type, number).subscribe(data => {
       this.rooms = data.rooms;
       this.count = data.count;
+     // this.niz1 = data.count;
+     this.niz3 = this.count;
+     this.myChart.Series = this.niz3;
     });
   }
   
