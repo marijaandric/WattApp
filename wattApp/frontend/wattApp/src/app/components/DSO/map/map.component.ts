9a91d316e149c9938b6df2698e40c8f1fbbe4798
Marija Andric <marijaandric2001@gmail.com>
2023-04-08 16:12:38 +0200

    Update map.component.ts

--- wattApp/frontend/wattApp/src/app/components/DSO/map/map.component.ts
+++ wattApp/frontend/wattApp/src/app/components/DSO/map/map.component.ts
@@ -28,7 +28,7 @@ export class MapComponent implements OnInit, OnChanges{
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors'
     }).addTo(this.map);
-    this.darkLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    this.darkLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', { //https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png
       attribution: '&copy; OpenStreetMap contributors'
     });
     this.lightLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
