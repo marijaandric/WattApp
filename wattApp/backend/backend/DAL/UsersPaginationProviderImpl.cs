1642b6c8c12f625de4c1d99cf2f5c26a7cf32c3e
Vanja Mijatovic <vmijatovic2000@gmail.com>
2023-05-07 18:56:48 +0300

    Added backend pagination for users table

---
+++ wattApp/backend/backend/DAL/UsersPaginationProviderImpl.cs
@@ -0,0 +1,28 @@
+﻿using backend.Context;
+using backend.DAL.Interfaces;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.DAL
+{
+    public class UsersPaginationProviderImpl : UsersPaginationProvider
+    {
+        private readonly AppDbContext _context;
+        public UsersPaginationProviderImpl(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<User> GetAllUsers(int pageNo, int pageSize, string sortOrder)
+        {
+            int offset = (pageNo - 1) * pageSize;
+            string query = $"SELECT * FROM Users ORDER BY {sortOrder} LIMIT {pageSize} OFFSET {offset}";
+            return _context.Users.FromSqlRaw(query).ToList();
+        }
+
+        public int GetAllUsersCount()
+        {
+            return _context.Users.Count();
+        }
+    }
+}
