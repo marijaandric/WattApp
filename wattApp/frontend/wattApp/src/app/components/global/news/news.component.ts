6c383bdbdd2d73ed7d586b228275fadf59afa3a6
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-05-08 11:46:12 +0200

    #81 News page design

--- wattApp/frontend/wattApp/src/app/components/global/news/news.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/news/news.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { DsonewsService } from 'src/app/services/dsonews/dsonews.service';
+import { CarouselModule } from 'primeng/carousel';
+import { DeviceDTO } from 'src/app/dtos/DeviceDTO';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit{
+
+  news: any[] = [];
+  newsImportant: any[] = [];
+  newsRegular: any[] = [];
+
+  constructor(private dsonew : DsonewsService) {
+  }
+
+  getNews() {
+    this.dsonew.getnew().subscribe((data: any) => {
+      this.news = data;
+
+     
+      data.forEach((element:any) => {
+        this.newsRegular.push(element)
+      });
+      this.newsRegular.sort((a, b) => b.id - a.id);
+
+      data.forEach((element:any) => {
+        this.newsImportant.push(element)
+      });
+      this.newsImportant.sort((a, b) => b.id - a.id);
+
+      //console.log(data);
+
+      console.log(this.news);
+      console.log(this.newsImportant);
+      console.log(this.newsRegular);
+      this.newsRegular.forEach((element,index)=>{
+        if(element.priority=== "Important") this.newsRegular.splice(index,1);
+      });
+      this.newsImportant.forEach((element,index)=>{
+        if(element.priority=== "Regular") this.newsImportant.splice(index,1);
+      });
+
+      console.log(this.news);
+      console.log(this.newsImportant);
+      console.log(this.newsRegular);
+    });
+  
+  }
+
+  ngOnInit(): void {
+    this.getNews();
+    
+  }
 
 }
