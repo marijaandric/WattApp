c6d4557d6efbf292d5eb3c7cb41e5b6eb68b76ce
pandur035 <stojanoviccaleksa@gmail.com>
2023-05-11 01:55:26 +0200

    bug fix

--- wattApp/backend/backend/BLL/DevicesAndDevicesDataBL.cs
+++ wattApp/backend/backend/BLL/DevicesAndDevicesDataBL.cs
@@ -187,7 +187,7 @@ namespace backend.BLL
             {
                 Console.WriteLine("Fake: " + dvcs.deviceID);
                 Console.WriteLine("True: +" + _contextDAL.GetDevice(dvcs.deviceID).Id);
-                devicesMap.Add(_contextDAL.GetDevice(dvcs.deviceID).Id, dvcs.usage);
+                devicesMap.Add(_contextDAL.GetDeviceIDForUserByFakeID(userId, dvcs.deviceID), dvcs.usage);
             }
 
             if (size.ToLower() == "max")
