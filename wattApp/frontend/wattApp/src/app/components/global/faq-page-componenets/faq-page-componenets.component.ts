239c9fcf92e0bef59e9fc4ee5eeb496e65dc93f5
Boris <dugicboris@gmail.com>
2023-05-12 16:35:16 +0200

    FAQ fixed

--- wattApp/frontend/wattApp/src/app/components/global/faq-page-componenets/faq-page-componenets.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/faq-page-componenets/faq-page-componenets.component.ts
@@ -3,17 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-faq-page-componenets',
   templateUrl: './faq-page-componenets.component.html',
-  styleUrls: ['./faq-page-componenets.component.css']
+  styleUrls: ['./faq-page-componenets.component.scss']
 })
 export class FaqPageComponenetsComponent {
   tabs = [
-    { title: 'How does the app differ from other similar products on the market?', content: 'The application provides users with detailed reports on their energy production and consumption, as well as information on electricity storage. Users can monitor their energy consumption in real time and identify areas where the most energy is consumed. The app also provides detailed analysis of greenhouse gas emissions, allowing users to better understand their impact on the environment.' },
+    { title: 'How does your energy management application work?', content: 'Our energy management application uses the Internet of Things (IoT) and artificial intelligence (AI) to collect, analyze and predict electricity consumption and production within a customers microgrid. Network managers will be able to monitor the behavior of all system users and analyze consumption and production, as well as the state of system components.' },
+    { title: 'Who can use your app?', content: 'Naša aplikacija za upravljanje energijom je dostupna za korišćenje svim korisnicima sistema, uključujući i upravljače mreže i potrošače.' },
+    { title: 'Can the app reduce the cost of your electricity bill?', content: 'Yes, our app can help reduce the cost of your electricity bill. By collecting information about electricity consumption and production, our app can identify areas where too much energy is being used and suggest ways to reduce consumption.' },
+    { title: 'Can the application predict future energy consumption?', content: 'Yes, our app uses artificial intelligence to analyze past patterns of energy consumption and production, in order to predict future consumption. This helps grid operators to plan and adjust the generation and distribution of electricity.' },
     { title: 'How can the production of energy from renewable sources be monitored?', content: 'Our application can monitor the production of energy from renewable sources through sensors for measuring production at different points in the system. This includes solar panels, wind farms and other sources of renewable energy.' },
     { title: 'What devices are supported for monitoring power consumption?', content: 'Our application supports a wide range of devices for monitoring power consumption, including power sensors, smart sockets, smart switches and other similar devices that are compatible with our system.' },
     { title: 'How does your application measure power consumption?', content: 'The application uses various sensors and devices to measure electricity consumption at different points in your home or business. This includes sensors for measuring current on main sockets, sensors for measuring consumption on individual devices and other technologies that are able to monitor the amount of electricity consumption.' },
     { title: 'What types of reports and analysis does the application provide?', content: 'The application provides various types of reports and analyses, including monthly power consumption reports, individual appliance consumption reports, energy savings reports, renewable energy production reports and other relevant information.' },
-    { title: 'How can the production of energy from renewable sources be monitored?', content: 'Our application can monitor the production of energy from renewable sources through sensors for measuring production at different points in the system. This includes solar panels, wind farms and other sources of renewable energy.' },
-    { title: 'How can alerts for high consumption or failures be configured?', content: 'The app allows users to set alerts for high consumption or outages via the mobile app or web interface. Users can also configure alerts via email or text messages to be notified of issues.' },
     { title: 'How is the security and privacy of user data ensured?', content: 'The security and privacy of our users data is very important to us. The application uses advanced encryption technologies and other security measures to ensure that all user data is protected. In addition, we adhere to a strict data privacy policy to ensure that our users data is always safe and secure.' },
     // add more tabs here
   ];
