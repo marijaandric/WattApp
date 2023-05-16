d64b00c82c785eaa5edaa233557808385961d5ce
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-05-16 12:51:08 +0200

    #111 Widgets for light mode

--- wattApp/frontend/wattApp/src/app/components/global/layout-components/widget/widget.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/layout-components/widget/widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostBinding, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-widget',
@@ -6,6 +6,8 @@ import { Component, HostBinding, Input, OnInit } from '@angular/core';
   styleUrls: ['./widget.component.css']
 })
 export class WidgetComponent implements OnInit{
+  hostElement: HTMLElement | undefined;
+
   @Input() subTitle : String ="Current price of electricity";
   @Input() Title : String ="9,051 din/kWh";
 
@@ -21,8 +23,38 @@ export class WidgetComponent implements OnInit{
   @Input() @HostBinding("orange-color") public isOrange = false;
   @Input() @HostBinding("bg-orange-color") public isBgOrange = false;
 
+  constructor (private elementRef: ElementRef, private renderer: Renderer2) {
+
+  }
+
+
   ngOnInit(): void {
-    
+    this.hostElement = this.elementRef.nativeElement as HTMLElement;
+    /*const innerElements = this.hostElement?.querySelectorAll('.main');
+    innerElements.forEach((innerElement) => {
+      this.renderer.addClass(innerElement, 'dark-theme-color-gray');
+    });
+  */
+    this.hostElement?.classList.add('light-theme-background-white');
+    const text = this.hostElement?.querySelector('.item_title');
+    this.renderer.addClass(text, 'ligh-theme-text-color-gray');
+    this.hostElement.addEventListener('mouseenter', this.onMouseEnter.bind(this));
+    this.hostElement.addEventListener('mouseleave', this.onMouseLeave.bind(this));
+  }
+
+
+  private onMouseEnter(): void {
+    const text = this.hostElement?.querySelector('.item_title');
+    if (text) {
+      text.classList.add('color-white');
+    }
+  }
+
+  private onMouseLeave(): void {
+    const text = this.hostElement?.querySelector('.item_title');
+    if (text) {
+      text.classList.remove('color-white');
+    }
   }
 
   
