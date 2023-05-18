87896eb0ba3454d9f514952489e5d9175caa8b76
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-05-18 11:52:58 +0200

    #111 tab users light mode

--- wattApp/frontend/wattApp/src/app/components/DSO/users-page-components/tab-menu-users/tab-menu-users.component.ts
+++ wattApp/frontend/wattApp/src/app/components/DSO/users-page-components/tab-menu-users/tab-menu-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { UserService } from 'src/app/services/user.service';
@@ -9,10 +9,23 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./tab-menu-users.component.css']
 })
 export class TabMenuUsersComponent implements OnInit{
+  
+  hostElement: HTMLElement | undefined;
   rola! : string;
-  constructor(private router: Router, private userService:UserService) {}
+  constructor(private router: Router, private userService:UserService, private elementRef: ElementRef, private renderer: Renderer2) {}
   ngOnInit(): void {
     this.role();
+
+    this.hostElement = this.elementRef.nativeElement as HTMLElement;
+    
+    const text = this.hostElement?.querySelectorAll('.tab-title');
+    text.forEach((innerElement) => {
+      this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
+    });
+    const icons = this.hostElement?.querySelectorAll('.tab-icon i');
+    icons.forEach((innerElement) => {
+      this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
+    });
   }
 
   goToAboutPage() {
