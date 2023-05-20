592b84641423d2dcdea388122ee2747504943ab4
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-05-21 01:35:34 +0200

    #111 about us final lm

--- wattApp/frontend/wattApp/src/app/components/global/about-us/about-us.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/about-us/about-us.component.ts
@@ -12,15 +12,22 @@ export class AboutUsComponent {
 
   }
 
-  ngOnIt() {
+  ngOnInit(): void {
     this.hostElement = this.elementRef.nativeElement as HTMLElement;
-   
     const cards = this.hostElement?.querySelectorAll('.klasa .cont .card');
     cards.forEach((card) => {
       this.renderer.addClass(card,'light-theme-background-white');
       this.renderer.addClass(card,'light-theme-bigger-shadow');
-      console.log(card);
     });
-    
+    const pEl = this.hostElement?.querySelectorAll('h5, p, h4');
+    pEl.forEach((innerElement) => {
+      this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
+    });
+    const info = this.hostElement?.querySelector('.info');
+    this.renderer.addClass(info,'light-theme-background-white');
+    this.renderer.addClass(info,'light-theme-bigger-shadow');
+    const image = this.hostElement?.querySelector('.image');
+    this.renderer.addClass(image,'light-theme-background-white');
+    this.renderer.addClass(image,'light-theme-bigger-shadow');
   }
 }
