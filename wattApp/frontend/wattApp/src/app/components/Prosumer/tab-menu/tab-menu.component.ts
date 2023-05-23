02e082a6fe232d38aa8075738de6e63d937c0493
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-05-24 00:21:14 +0200

    #111 tab menu for users light mode

--- wattApp/frontend/wattApp/src/app/components/Prosumer/tab-menu/tab-menu.component.ts
+++ wattApp/frontend/wattApp/src/app/components/Prosumer/tab-menu/tab-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-tab-menu',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab-menu.component.css']
 })
 export class TabMenuComponent {
+  hostElement: HTMLElement | undefined;
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  
 
+  ngOnInit(): void {
+
+    this.hostElement = this.elementRef.nativeElement as HTMLElement;
+    
+    const text = this.hostElement?.querySelectorAll('.tab-title');
+    text.forEach((innerElement) => {
+      this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
+    });
+    const icons = this.hostElement?.querySelectorAll('.tab-icon i');
+    icons.forEach((innerElement) => {
+      this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
+    });
+  }
 }
