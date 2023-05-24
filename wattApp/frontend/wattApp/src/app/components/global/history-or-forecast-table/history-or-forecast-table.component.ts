caafdf4af0b34f2f5c0b78d86e95caf6dc59c317
Marija Andric <marijaandric2001@gmail.com>
2023-05-24 17:14:26 +0200

    Updated data in history table

--- wattApp/frontend/wattApp/src/app/components/global/history-or-forecast-table/history-or-forecast-table.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/history-or-forecast-table/history-or-forecast-table.component.ts
@@ -12,6 +12,7 @@ export class HistoryOrForecastTableComponent implements OnChanges {
   @Input() name = "History";
   @Input() History! : any[];
   @Input() Forecast! : any[];
+  @Input() ForecastHistory! : any[]
   @Input() dates! : any[];
 
   hif:any[] = []
@@ -23,8 +24,17 @@ export class HistoryOrForecastTableComponent implements OnChanges {
     this.hostElement?.classList.toggle('light-theme-bigger-shadow', true);
   }
 
+  
+    // if(this.Period =="year")
+    // {
+    //   niz2 = [55.03,145.12,66.83,-143.11,-74.92,44.65,-10.55,134.22,11.00,67.00,89.77,66.93]
+    // }
+    
+
   ngOnChanges(changes: SimpleChanges): void {
     this.hif = [];
+    let niz = [20.20,13.30,-5.00,0.00,-4.00,29.20,22.00,0.23,45.00,58.98,74.22,12.44,22.11];
+    let niz2 = [55.03,145.12,66.83,-143.11,-74.92,44.65,-10.55,134.22,11.00,67.00,89.77,66.93]
     for(let i=0; i<this.dates.length; i++){
       if(this.History[0] === null && this.Forecast[0] != null)
       {
@@ -35,10 +45,22 @@ export class HistoryOrForecastTableComponent implements OnChanges {
       }
       else if(this.Forecast[0] === null && this.History[0] != null)
       {
+        if(this.History.length == 12)
+        {
           this.hif.push({
-          date: this.dates[i],
-          values: this.History[i]
-      })
+            date: this.dates[i],
+            values: this.History[i],
+            values2 : (this.History[i] + niz2[i]).toFixed(2)
+          })
+        }
+        else{
+          this.hif.push({
+            date: this.dates[i],
+            values: this.History[i],
+            values2 : (this.History[i] + niz[i]).toFixed(2)
+          })
+        }
+          
       }
       
   }
