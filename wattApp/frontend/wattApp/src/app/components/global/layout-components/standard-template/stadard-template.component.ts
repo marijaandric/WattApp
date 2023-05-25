a52d659ab7361d5271b5e896d198efe745c6d5f2
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-05-25 23:01:54 +0200

    #133 standard template bg color

--- wattApp/frontend/wattApp/src/app/components/global/layout-components/standard-template/stadard-template.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/layout-components/standard-template/stadard-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'standard-template',
@@ -8,9 +9,17 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
 export class StadardTemplateComponent {
   hostElement: HTMLElement | undefined;
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2, private userService: UserService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.hostElement = this.elementRef.nativeElement as HTMLElement;
+    
+    const token = localStorage.getItem('token');
+    this.userService.isDark$.subscribe(dark => {
+      this.hostElement?.classList.toggle('dark-theme-application-background', dark);
+      this.hostElement?.classList.toggle('light-theme-application-background', !dark);
+    });
+
     this.hostElement = this.elementRef.nativeElement as HTMLElement;
     console.log(':host selector:', this.hostElement.tagName.toLowerCase());
     const sidebar = this.hostElement?.querySelector('.sidebar');
