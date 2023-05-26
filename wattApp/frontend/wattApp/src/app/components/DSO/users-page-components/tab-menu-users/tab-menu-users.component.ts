3f64eb50dcc19645199a5a3ab0c33359fa8c222b
Denis Dragoljubovic <denidragoljubovic123@gmail.com>
2023-05-26 15:36:52 +0200

    #133 tab menu users

--- wattApp/frontend/wattApp/src/app/components/DSO/users-page-components/tab-menu-users/tab-menu-users.component.ts
+++ wattApp/frontend/wattApp/src/app/components/DSO/users-page-components/tab-menu-users/tab-menu-users.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { UserService } from 'src/app/services/user.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-tab-menu-users',
@@ -9,23 +9,45 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./tab-menu-users.component.css']
 })
 export class TabMenuUsersComponent implements OnInit{
-  
+  lightMode:Boolean = false;
   hostElement: HTMLElement | undefined;
   rola! : string;
   constructor(private router: Router, private userService:UserService, private elementRef: ElementRef, private renderer: Renderer2) {}
-  ngOnInit(): void {
+  
+  async ngOnInit(): Promise<void> {
+
+    const token = localStorage.getItem('token');
+    this.userService.isDark$.subscribe(dark => {
+      this.lightMode = !dark;
+      if(!dark) {
+        this.hostElement = this.elementRef.nativeElement as HTMLElement;
+        const text = this.hostElement?.querySelectorAll('.tab-title');
+        text.forEach((innerElement) => {
+          this.renderer.removeClass(innerElement, 'color-white');
+          this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
+        });
+        const icons = this.hostElement?.querySelectorAll('.tab-icon i');
+        icons.forEach((innerElement) => {
+          this.renderer.removeClass(innerElement, 'color-white');
+          this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
+        });
+      }
+     else {
+        this.hostElement = this.elementRef.nativeElement as HTMLElement;
+        const text = this.hostElement?.querySelectorAll('.tab-title');
+        text.forEach((innerElement) => {
+          this.renderer.removeClass(innerElement, 'ligh-theme-text-color-gray');
+          this.renderer.addClass(innerElement, 'color-white');
+        });
+        const icons = this.hostElement?.querySelectorAll('.tab-icon i');
+        icons.forEach((innerElement) => {
+          this.renderer.removeClass(innerElement, 'ligh-theme-text-color-gray');
+          this.renderer.addClass(innerElement, 'color-white');
+        });
+      }
+    });
     this.role();
 
-    this.hostElement = this.elementRef.nativeElement as HTMLElement;
-    
-    const text = this.hostElement?.querySelectorAll('.tab-title');
-    text.forEach((innerElement) => {
-      this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
-    });
-    const icons = this.hostElement?.querySelectorAll('.tab-icon i');
-    icons.forEach((innerElement) => {
-      this.renderer.addClass(innerElement, 'ligh-theme-text-color-gray');
-    });
   }
 
   goToAboutPage() {
--- wattApp/frontend/wattApp/src/app/components/global/layout-components/select-one-menu-bar/select-one-menu-bar.component.ts
+++ wattApp/frontend/wattApp/src/app/components/global/layout-components/select-one-menu-bar/select-one-menu-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-select-one-menu-bar',
@@ -9,15 +10,21 @@ export class SelectOneMenuBarComponent implements OnInit{
 
   hostElement: HTMLElement | undefined;
 
-  constructor (private elementRef: ElementRef, private renderer: Renderer2) {
+  constructor (private elementRef: ElementRef, private renderer: Renderer2, private userService: UserService) {
 
   }
 
-  ngOnInit():void {
-    this.hostElement = this.elementRef.nativeElement as HTMLElement;
-    this.hostElement?.classList.toggle('light-theme-bigger-shadow', true);
-    this.hostElement?.classList.add('light-theme-background-white');
-
+  async ngOnInit(): Promise<void> {
+    
+    const token = localStorage.getItem('token');
+    this.userService.isDark$.subscribe(dark => {
+      this.hostElement = this.elementRef.nativeElement as HTMLElement;
+      
+      this.hostElement?.classList.toggle('dark-theme-bigger-shadow', dark);
+      this.hostElement?.classList.toggle('light-theme-bigger-shadow', !dark);
+      this.hostElement?.classList.toggle('dark-theme-background-gray-gradient-1', dark);
+      this.hostElement?.classList.toggle('light-theme-background-white', !dark);
+    });
   }
 
 }
