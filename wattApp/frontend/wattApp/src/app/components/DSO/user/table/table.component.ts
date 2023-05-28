23569dc38369b8cfbccc5d50c91c4e5d9ade3de9
Marija Andric <marijaandric2001@gmail.com>
2023-05-28 14:03:48 +0200

    Filtering in the UserDSO page

--- wattApp/frontend/wattApp/src/app/components/DSO/user/table/table.component.ts
+++ wattApp/frontend/wattApp/src/app/components/DSO/user/table/table.component.ts
@@ -3,6 +3,8 @@ import { DeviceDataService } from 'src/app/services/device-data/device-data.serv
 import { DeviceService } from 'src/app/services/device/device.service';
 import { lastValueFrom } from 'rxjs';
 import { UserService } from 'src/app/services/user/user.service';
+import { Table } from 'primeng/table';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-table',
@@ -12,11 +14,30 @@ import { UserService } from 'src/app/services/user/user.service';
 export class TableComponent implements OnInit{
   lightMode : Boolean = true;
   @ViewChild('searchInput') searchInput!: ElementRef;
+  @ViewChild('dtUsers', { static: false }) table!: Table;
   devices: any;
   @Input() id : any;
   isChecked: boolean = true;
 
-  constructor(private deviceService:DeviceService, private userService: UserService){}
+  statusFilterOptions: SelectItem[];
+  selectedStatusFilter: any;
+
+  activityFilterOptions: SelectItem[];
+  selectedActivityFilter: any;
+
+  constructor(private deviceService:DeviceService, private userService: UserService)
+  {
+    this.statusFilterOptions = [
+      { label: 'Consumer', value: 'Consumer' },
+      { label: 'Producer', value: 'Producer' },
+      { label: 'Stock', value: 'Stock' }
+    ];
+
+    this.activityFilterOptions = [
+      { label: 'true', value: 'true' },
+      { label: 'false', value: 'false' }
+    ];
+  }
 
 
   async ngOnInit(): Promise<void> {
@@ -42,5 +63,33 @@ export class TableComponent implements OnInit{
 
   async handleRunningSwitchChange(device : any){
     await lastValueFrom(this.deviceService.updateUserDSODevice(device,this.id));
+  }
+
+  filterStatus(value: any) {
+    this.table.filter(value, 'deviceType', 'equals');
+  }
+
+  getSeverity(label: string):string {
+    if (label === 'Consumer') {
+      return 'Consumer';
+    } else if (label === 'Producer') {
+      return 'Producer';
+    } else if (label === 'Stock') {
+      return 'Stock';
+    } else {
+      return 'All';
+    }
+  }
+
+  filterActivity(value: any) {
+    this.table.filter(value, 'isActive', 'equals');
+  }
+
+  getSeverityActivity(label: string):string {
+    if (label === 'true') {
+      return 'true';
+    } else {
+      return 'false';
+    }
   }
 }
